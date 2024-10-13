99e363fd5868b4d0dfd0b8ff447b56a0559b27bc
涂志伟 <799214697@qq.com>
2024-10-13 12:06:55 +0800

    Feat: 优化打包

--- app/pages/layout/homeNtf.jsx
+++ app/pages/layout/homeNtf.jsx
@@ -1,16 +1,9 @@
-'use client'
-import { cn } from '../../../lib/utils'
-import Marquee from '../../..//components/ui/marquee'
-import React, { useState, useEffect } from 'react'
-import {
-  Address,
-  NFTImage,
-  Connector,
-  ConnectButton,
-  useAccount,
-  useProvider,
-} from '@ant-design/web3'
-const network_id = '1' // 参考 <https://docs.chainbase.com/reference/supported-chains> 获取不同网络的 ID。
+'use client';
+import { NFTImage } from '@ant-design/web3';
+import { useState } from 'react';
+import Marquee from '../../..//components/ui/marquee';
+import { cn } from '../../../lib/utils';
+const network_id = '1'; // 参考 <https://docs.chainbase.com/reference/supported-chains> 获取不同网络的 ID。
 
 const ReviewCard = ({ id, type }) => {
   return (
@@ -20,7 +13,7 @@ const ReviewCard = ({ id, type }) => {
         // light styles
         'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
         // dark styles
-        'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]',
+        'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
       )}
     >
       <NFTImage
@@ -33,11 +26,11 @@ const ReviewCard = ({ id, type }) => {
         width={255}
       />
     </figure>
-  )
-}
+  );
+};
 
 export function HomeNtf() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   const reviews = [
     {
@@ -61,18 +54,18 @@ export function HomeNtf() {
     {
       id: '2000',
     },
-  ]
+  ];
   const getNTF = (type, token_id = '1') => {
-    const aku_contract_addr = '0xed5af388653567af2f388e6224dc7c4b3241c544' // 以 Azuki 合约地址为例。
-    const bayc_contract_addr = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+    const aku_contract_addr = '0xed5af388653567af2f388e6224dc7c4b3241c544'; // 以 Azuki 合约地址为例。
+    const bayc_contract_addr = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d';
 
-    const CHAINBASE_API_KEY = '2nKxVJ2feS2ZUYc8ajB7cSBqu4Y'
-  }
+    const CHAINBASE_API_KEY = '2nKxVJ2feS2ZUYc8ajB7cSBqu4Y';
+  };
 
-  const firstRow = reviews.slice(0, reviews.length)
-  const secondRow = reviews.slice(0, reviews.length)
+  const firstRow = reviews.slice(0, reviews.length);
+  const secondRow = reviews.slice(0, reviews.length);
   return (
-    <div className="next_ntf_box absolute flex h-[400px] w-full flex-col items-center  overflow-hidden rounded-lg  bg-background">
+    <div className="next_ntf_box absolute flex h-[400px] w-full flex-col items-center overflow-hidden rounded-lg bg-background">
       <Marquee pauseOnHover className="[--duration:20s]">
         {firstRow.map((review) => (
           <ReviewCard key={review.id} {...review} type="aku" />
@@ -86,5 +79,5 @@ export function HomeNtf() {
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
     </div>
-  )
+  );
 }
